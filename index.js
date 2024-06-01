654f03d69e1d13b4c91a8ac83ff8633ba9a4b4b1
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-06-01 23:39:07 +0530

    ui

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let emoji = document.getElementById("emoji")
+let des = document.getElementById("des")
+let result;
+async function fetchEmoji(){
+    let res = await fetch('https://emoji-api.com/emojis?access_key=bc7d37e67bbbb04ce5f8e2502cc0f0ca569cb323')
+    result = await res.json();
+    // console.log(result)
+}
+
+fetchEmoji()
 
 emoji.addEventListener('click', () => {
-    console.log("first")
+    let num = Math.floor(Math.random()*result.length)
+    // console.log(num)
+    emoji.innerHTML = result[num].character
+    des.innerHTML = result[num].unicodeName.split('.')[1].substring('2')
 })
